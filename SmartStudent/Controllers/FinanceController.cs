86f4446ea51cd2154f5bde55c6bbb8e49f459478
Alexander Minchev <minchev.alexm@gmail.com>
2025-12-17 15:35:51 +0200

    Fixed missing styles.css

--- SmartStudent/Controllers/FinanceController.cs
+++ SmartStudent/Controllers/FinanceController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using SmartStudent.Data;
+using SmartStudent.Models;
 
 namespace SmartStudent.Controllers
 {
@@ -23,5 +24,59 @@ namespace SmartStudent.Controllers
             var transactions = await db.Transactions.ToListAsync();
             return View(transactions);
         }
+
+        public async Task<IActionResult> ViewTransaction(int id)
+        {
+            var transaction = await db.Transactions.FirstOrDefaultAsync(t => t.Id == id);
+            if (transaction == null)
+            {
+                return NotFound();
+            }
+
+            return View(transaction);
+        }
+
+        [HttpGet]
+        public IActionResult CreateTransaction()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateTransaction(Transaction transaction, IFormFile DocumentPath)
+        {
+            if (ModelState.IsValid)
+            {
+                if (DocumentPath != null && DocumentPath.Length > 0)
+                {
+                    var uploadsFolder = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "documents");
+                    if (!Directory.Exists(uploadsFolder))
+                    {
+                        Directory.CreateDirectory(uploadsFolder);
+                    }
+
+                    //Generate unique file name
+                    var uniqueFileName = Guid.NewGuid().ToString() + Path.GetExtension(DocumentPath.FileName);
+                    var filePath = Path.Combine(uploadsFolder, uniqueFileName);
+
+                    //Save to disk
+                    using (var stream = new FileStream(filePath, FileMode.Create))
+                    {
+                        await DocumentPath.CopyToAsync(stream);
+                    }
+
+                    //Store relative path
+                    transaction.DocumentPath = $"/documents/{uniqueFileName}";
+                }
+
+                db.Transactions.Add(transaction);
+                await db.SaveChangesAsync();
+                return RedirectToAction(nameof(Transactions));
+            }
+
+            return View(transaction);
+        }
+
+
     }
 }
